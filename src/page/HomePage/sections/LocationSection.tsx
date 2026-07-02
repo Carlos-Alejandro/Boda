598cdc2b94c3fd46e291b98b2f3bac5502bf55ca
CommunityManager1884 <digitalmarketing@vnvgroup.com>
2026-07-02 17:37:15 -0500

    Fix church animation import case

--- src/page/HomePage/sections/LocationSection.tsx
+++ src/page/HomePage/sections/LocationSection.tsx
@@ -5,8 +5,8 @@ import { MapPin } from 'lucide-react';
 
 import florInicioUp from '../../../assets/story/flor-inicio-up.png';
 import florInicioDown from '../../../assets/story/flor-inicio-down.png';
-import churchAnimation from '../../../assets/animations/church.json';
-import celebrationAnimation from '../../../assets/animations/celebration.json';
+import churchAnimation from '../../../assets/animations/Church.json';
+import celebrationAnimation from '../../../assets/animations/Celebration.json';
 
 export function LocationSection() {
 	const churchRef = useRef<Player>(null);
